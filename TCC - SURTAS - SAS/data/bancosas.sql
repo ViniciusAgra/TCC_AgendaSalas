39aa48e894537323842b131dd1efff6a3eb3440e
Sofia Vasconcellos de Carvalho <151774011+vasconcellosofia@users.noreply.github.com>
2024-09-18 09:10:08 -0300

    Atualização 4.1 - Alteração no código do banco de dados

--- TCC - SURTAS - SAS/data/bancosas.sql
+++ TCC - SURTAS - SAS/data/bancosas.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS DBSAS;
 USE DBSAS;
-	
+
+
 CREATE TABLE IF NOT EXISTS `usuario` (
   `Prontuario` varchar(10) NOT NULL,
   `Nome` varchar(45) NOT NULL,
@@ -11,23 +12,19 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 );
 
 CREATE TABLE IF NOT EXISTS `turma` (
-  `ID_Turma` int NOT NULL AUTO_INCREMENT,
-  `Periodo_ou_ano` int NOT NULL,
-  `Curso` varchar(50) NOT NULL,
-  PRIMARY KEY (`ID_Turma`)
+  `Nome_Turma` varchar(40) NOT NULL,
+  PRIMARY KEY (`Nome_Turma`)
 );
 
-CREATE TABLE IF NOT EXISTS `sala` (
-  `ID_Sala` int NOT NULL,
-  `Bloco` int NOT NULL,
-  `Nome` varchar(30) NOT NULL,
-  PRIMARY KEY (`ID_Sala`)
+CREATE TABLE `sala` (
+  `Nome_Sala` varchar(40) NOT NULL,
+  PRIMARY KEY (`Nome_Sala`)
 );
 
-CREATE TABLE IF NOT EXISTS `reserva` (
+CREATE TABLE `reserva` (
   `ID_Reserva` int NOT NULL AUTO_INCREMENT,
-  `Sala` int NOT NULL,
-  `Turma` int NOT NULL,
+  `Sala` varchar(40) NOT NULL,
+  `Turma` varchar(40) NOT NULL,
   `Dia` date NOT NULL,
   `Professor` varchar(10) NOT NULL,
   `Professor_2` varchar(10) DEFAULT NULL,
@@ -35,12 +32,12 @@ CREATE TABLE IF NOT EXISTS `reserva` (
   `Fim` time NOT NULL,
   `Descricao` varchar(45) NOT NULL,
   PRIMARY KEY (`ID_Reserva`),
-  KEY `ID_Sala_idx` (`Sala`),
-  KEY `ID_Turma_idx` (`Turma`),
+  KEY `Nome_Sala_idx` (`Sala`),
+  KEY `Nome_Turma_idx` (`Turma`),
   KEY `Professor_idx` (`Professor`),
   KEY `Professor_2_idx` (`Professor_2`),
-  CONSTRAINT `ID_Sala` FOREIGN KEY (`Sala`) REFERENCES `sala` (`ID_Sala`),
-  CONSTRAINT `ID_Turma` FOREIGN KEY (`Turma`) REFERENCES `turma` (`ID_Turma`),
+  CONSTRAINT `Nome_Sala` FOREIGN KEY (`Sala`) REFERENCES `sala` (`Nome_Sala`),
+  CONSTRAINT `Nome_Turma` FOREIGN KEY (`Turma`) REFERENCES `turma` (`Nome_Turma`),
   CONSTRAINT `Professor` FOREIGN KEY (`Professor`) REFERENCES `usuario` (`Prontuario`),
   CONSTRAINT `Professor_2` FOREIGN KEY (`Professor_2`) REFERENCES `usuario` (`Prontuario`)
 );
